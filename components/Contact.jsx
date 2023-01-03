aebccf7492e65e30a77d63b5527e63f0be53fca7
Athina oluwafunke Owoeye <athinaoluwafunkeowoeye@MacBook-Pro-som-tillhor-Athina.local>
2023-01-03 18:05:12 +0100

    created contact page

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,4 +1,10 @@
 import React from 'react'
+import Image from 'next/image';
+import { AiOutlineMail } from 'react-icons/ai'
+import { BsFillPersonFill } from 'react-icons/bs'
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa'
+
+import ath from '../public/ath-wave.png'
 
 const Contact = () => {
   return (
@@ -6,10 +12,54 @@ const Contact = () => {
          <div className="max-w-[1240px] m-auto px-2 py-16 w-full">
        <p className=" text-xl tracking-widest uppercase text-indigo-500">Contact</p>
        <h2 className="py-4">Get In Touch</h2>
-       <div>
+       <div className="grid lg:grid-cols-5 gap-8">
+   {/* left section */}
+      <div className="col-span-3 lg:col-span-2 w-full h-full shadow-md shadow-gray-400 rounded-md">
+        <div className="lg:p-4 h-full">
+           <div>
+               <Image className="hover:scale-105 ease-in duration-300" src={ath}/>
+           </div>
+           <div>
+               <h2 className="py-2">Athina Owoeye</h2>
+               <p>Front-End Developer</p>
+           </div>
+           <div>
+               <div>
+                   <p className="pt-8">CONNECT WITH ME</p>
+                   <div className="flex items-center justify-between py-4">
+                   <div className="rounded-full shadow-md shadow-gray-400 p-6 cursor-pointer hover:scale-105 ease-in duration-300">
+                         <FaLinkedinIn />
+                     </div>
+                     <div className="rounded-full shadow-md shadow-gray-400 p-6 cursor-pointer hover:scale-105 ease-in duration-300">
+                         <FaGithub />
+                     </div>
+                     <div className="rounded-full shadow-md shadow-gray-400 p-6 cursor-pointer hover:scale-105 ease-in duration-300">
+                         <AiOutlineMail />
+                     </div>
+                     <div className="rounded-full shadow-md shadow-gray-400 p-6 cursor-pointer hover:scale-105 ease-in duration-300">
+                         <BsFillPersonFill/>
+                     </div>
+                   </div>
+               </div>
+           </div>
+        </div>
+      </div>
+
+      {/* right section */}
+         <div className="col-span-3 w-full h-auto shadow-md shadow-gray-400 rounded-md lg:py-4">
+             <div className="py-4">
+              <form>
+                  <div className="grid md:grid-cols-2 gap-4 w-full py-2">
+                      <div className="flex flex-col">
+                          <label></label>
+                      </div>
+                  </div>
+              </form>
+             </div> 
+         </div>
 
        </div>
-         </div>
+     </div>
     </div>
   );
 };
